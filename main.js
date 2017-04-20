b5c37abf4572662fdff34a9c354f2103b43edd19
Jason Pau <jason@jasonpau.com>
2017-04-19 20:21:16 -0700

    Memory Match version0.5 - Jason Pau

--- main.js
+++ main.js
@@ -2,23 +2,64 @@
  * Created by Jason on 4/19/2017.
  */
 
-var firstCardClicked = null;
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 4;
+var match_counter = 0;
+var temporarily_disable_clicks = false;
 
 $(document).ready(function(){
-    console.log('document is ready');
-
-    initializeEventHandlers();
+    initialize_event_handlers();
+    reset_game();
 });
 
-function initializeEventHandlers() {
-    $('.card').click(function() {
-        if (firstCardClicked === null) {
-            console.log('.card has been clicked, and firstCardClicked was null.');
-            $(this).find('.back').addClass("hidden");
-            firstCardClicked = this;
-        } else {
-            console.log('.card has been clicked, and firstCardClicked was NOT null.');
+function reset_game() {
+    $('.card').attr('status','in_gameplay');
+}
 
+function initialize_event_handlers() {
+    $('.card').click(card_clicked);
+}
+
+function card_clicked() {
+    // if we're currently waiting for the current round to reset, skip the rest of the function
+    if (temporarily_disable_clicks) {
+         return;
+    } else if ($(this).attr('status') === 'out_of_gameplay') {
+         return;
+    }
+
+    $(this).find('.back').addClass("hidden");
+
+    if (first_card_clicked === null) {
+        first_card_clicked = this;
+        $(this).attr('status','out_of_gameplay'); // temporarily take the card out of gameplay
+    } else {
+        second_card_clicked = this;
+        $(this).attr('status','out_of_gameplay'); // temporarily take the card out of gameplay
+
+        if ($(first_card_clicked).find('img').attr('src') === $(second_card_clicked).find('img').attr('src')) {
+            $(first_card_clicked).attr('status','out_of_gameplay'); // take the card out of gameplay after a match
+            $(second_card_clicked).attr('status','out_of_gameplay');
+            match_counter++;
+            first_card_clicked = null;
+            second_card_clicked = null;
+            if (match_counter === total_possible_matches) {
+                setTimeout(function () {
+                    alert('YOU WON TEH GAMEZ!!!!!1');
+                }, 50);
+            }
+        } else {
+            temporarily_disable_clicks = true;
+            setTimeout(function() {
+                $(first_card_clicked).find('.back').removeClass('hidden');
+                $(second_card_clicked).find('.back').removeClass('hidden');
+                $(first_card_clicked).attr('status','in_gameplay'); // put cards back into gameplay if not a match
+                $(second_card_clicked).attr('status','in_gameplay');
+                first_card_clicked = null;
+                second_card_clicked = null;
+                temporarily_disable_clicks = false;
+            }, 2000);
         }
-    });
+    }
 }
